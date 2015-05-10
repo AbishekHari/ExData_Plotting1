84101aafce95d22272d0048df3f58090af0aa964
Abishek Hari <hariabishek@gmail.com>
2015-05-11 03:06:33 +0530

    first version

---
+++ plot4.R
@@ -0,0 +1,23 @@
+
+# function to plot a histogram to depict global active power usage in KW
+plot4 <- function(src = "household_power_consumption.txt")
+{
+    # check if the source exists in the current working directory
+    if (!file.exists(src))
+        stop(src + " file doesnt exist")
+    
+    # read data into data frame
+    DT <- read.table("household_power_consumption.txt", sep=";", 
+                     stringsAsFactors=FALSE, na.strings="?", header = TRUE)    
+    
+    # filter based on required dates
+    DT <- DT[as.Date(DT$Date, "%d/%m/%Y") == as.Date("2007-02-01", "%Y-%m-%d") |
+                 as.Date(DT$Date, "%d/%m/%Y") == as.Date("2007-02-02", "%Y-%m-%d"),]
+    
+    # open PNG device with req width and height
+    png("plot4.png", width = 480, height=480)
+    
+    # plot the graph
+    
+    dev.off()
+}
